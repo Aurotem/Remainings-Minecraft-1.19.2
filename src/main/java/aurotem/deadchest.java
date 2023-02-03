df0cfd10d9620708ab39e815ad18b497d919d82f
Aurotem <melihcan.fi@gmail.com>
2023-02-03 19:43:09 +0300

    Bugs Fixed, the plugin now works successfully.

--- src/main/java/aurotem/deadchest.java
+++ src/main/java/aurotem/deadchest.java
@@ -17,12 +17,13 @@ import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.DoubleChestInventory;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.Objects;
+import java.util.Collection;
 
 
 public final class deadchest extends JavaPlugin implements Listener {
 
     int playerDeathExp = 0;
+    int playerCurrentExp = 0;
     String broadCastMessage = "§4 ---Dead Chest Plugin---\n" +
             "Author: Aurotem";
 
@@ -60,8 +61,11 @@ public final class deadchest extends JavaPlugin implements Listener {
                 p.sendMessage("Fakir bir şekilde öldün, " + playerName + "!");
                 return;
             }
+
+            int itemCount = deathEvent.getDrops().size();
+
             deathEvent.getDrops().clear(); //Clear the drops
-            deathEvent.setDroppedExp(0);
+            deathEvent.setDroppedExp(0); //Clear the exp
 
             //Creating a chest
             Block chestL = deathLoc.getBlock();
@@ -88,7 +92,6 @@ public final class deadchest extends JavaPlugin implements Listener {
 
             org.bukkit.block.Chest lChest = (org.bukkit.block.Chest) chestL.getState();
             org.bukkit.block.Chest rChest = (org.bukkit.block.Chest) chestR.getState();
-
             lChest.setCustomName(ChatColor.GOLD + playerName + ChatColor.RED + "'in Kalıntıları");
             rChest.setCustomName(ChatColor.GOLD + playerName + ChatColor.RED + "'in Kalıntıları");
             rChest.update();
@@ -105,41 +108,40 @@ public final class deadchest extends JavaPlugin implements Listener {
 
             //Creating Holograms.
             createHologram(ChatColor.RED + playerName + "' in olum yeri!", 0, lChest.getLocation().add(0.25,0,0), p);
-            createHologram("Item Sayisi: " + inventory.getContents().length, -0.5,lChest.getLocation().add(0.25,0,0), p);
-
+            createHologram("Item Sayisi: " + itemCount, -0.5,lChest.getLocation().add(0.25,0,0), p);
 
     }
 
 
     @EventHandler
-    public void OnRightClick(PlayerInteractEvent e){
+    public void OnRightClick(PlayerInteractEvent e) {
+        if (e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+            Block block = e.getClickedBlock();
 
-        Block block = e.getClickedBlock();
-        org.bukkit.block.Chest chest = (org.bukkit.block.Chest) block.getState();
-        if(e.getAction() == Action.LEFT_CLICK_BLOCK){
-            if(e.getClickedBlock().getType() == Material.CHEST){
-                if(chest.getCustomName().contains((e.getPlayer().getName()))){
-                breakChest(block);
-                }else if(!(chest.getCustomName().contains((e.getPlayer().getName())))){
-                e.getPlayer().sendMessage("Bu senin sandığın değil!");
-                }
-            }
-        }
-        if(e.getAction() == Action.RIGHT_CLICK_BLOCK){
-            if(e.getClickedBlock().getType() == Material.CHEST){
-                if(chest.getCustomName().contains((e.getPlayer().getName()))){
-                e.getPlayer().giveExpLevels(playerDeathExp);
-                playerDeathExp = 0;
+            if(e.getClickedBlock() != null) {
+                if (e.getClickedBlock().getType() == Material.CHEST) {
 
-                getServer().dispatchCommand(getServer().getConsoleSender(), "minecraft:kill \" +  e.getPlayer().getName() + \"[type=armor_stand,distance=..3]");
+                    assert block != null;
+                    org.bukkit.block.Chest chest = (org.bukkit.block.Chest) block.getState();
 
-                }else if(!(Objects.requireNonNull(chest.getCustomName()).contains((e.getPlayer().getName())))){
-                    e.getPlayer().sendMessage("Bu senin sandığın değil!");
-                    chest.close();
+                        if (chest.getCustomName() != null) {
+
+                            if (chest.getCustomName().contains((e.getPlayer().getName()))) {
+                                playerCurrentExp = e.getPlayer().getLevel();
+                                e.getPlayer().setLevel(playerCurrentExp + playerDeathExp);
+                                playerDeathExp = 0;
+                                playerCurrentExp = 0;
+                                breakChest(block);
+                            } else if (!(chest.getCustomName().contains((e.getPlayer().getName())))) {
+                                e.getPlayer().sendMessage("Bu senin sandığın değil!");
+                                chest.close();
+                            }
+                        }
                 }
             }
         }
     }
+
 
 
     public void createHologram(String Text, double y, Location location, Player p){
@@ -152,9 +154,32 @@ public final class deadchest extends JavaPlugin implements Listener {
     }
 
     private void breakChest(Block b){
+        org.bukkit.block.data.type.Chest chest = (org.bukkit.block.data.type.Chest) b.getBlockData();
+
+        if(chest.getType() == Chest.Type.LEFT){
+            Block block = b.getLocation().add(0,0,1).getBlock();
+            block.breakNaturally();
+        } else if(chest.getType() == Chest.Type.RIGHT){
+            Block block = b.getLocation().add(0,0,-1).getBlock();
+            block.breakNaturally();
+        }
+
         b.breakNaturally();
+
+
         b.getWorld().playSound(b.getLocation(), Sound.ENTITY_CHICKEN_EGG, 2, 1);
-        b.getWorld().spawnParticle(Particle.EXPLOSION_HUGE, b.getLocation().add(0,1,0), 1);
+
+        Collection<Entity> holograms = b.getWorld().getNearbyEntities(b.getLocation(),2,3,5);
+    try {
+        for (Entity entity : holograms) {
+            if(entity.getCustomName() != null)
+        ((org.bukkit.entity.ArmorStand) entity).setHealth(0);
+        }
+    }catch (ClassCastException ignored){
+
+    }
+
+
     }
 
 
